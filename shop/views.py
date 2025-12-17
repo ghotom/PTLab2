1d0c39d451b4285831f50d1363b2f1e29e1cf273
Anton Ostapko <ghotom1@mail.ru>
2025-12-18 00:04:41 +0300

    add func and visual of toy store

--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
+from .models import Product, Purchase, PromoCode
+from .forms import PromoCodeForm
 
-from .models import Product, Purchase
-
-# Create your views here.
 def index(request):
     products = Product.objects.all()
-    context = {'products': products}
+    
+    form = PromoCodeForm(request.GET or None)
+    discount = 0
+    error_message = None
+    
+    if form.is_valid():
+        code = form.cleaned_data.get('code')
+        if code:
+            try:
+                promo = PromoCode.objects.get(code__iexact=code, active=True)
+                discount = promo.discount_percent
+            except PromoCode.DoesNotExist:
+                error_message = "Неверный или неактивный промокод"
+
+    if discount > 0:
+        for product in products:
+            product.discounted_price = product.price * (100 - discount) // 100
+    else:
+        for product in products:
+            product.discounted_price = product.price
+
+    context = {
+        'products': products,
+        'form': form,
+        'discount': discount,
+        'error_message': error_message,
+    }
     return render(request, 'shop/index.html', context)
 
 
@@ -18,4 +43,3 @@ class PurchaseCreate(CreateView):
     def form_valid(self, form):
         self.object = form.save()
         return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
-
