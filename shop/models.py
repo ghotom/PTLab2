acadbbe075db05733c76d1a7f055851ba29a2907
Anton Ostapko <ghotom1@mail.ru>
2025-12-18 00:00:01 +0300

    add classes Product Purchase and Promocode to models.py

--- shop/models.py
+++ shop/models.py
@@ -10,3 +10,11 @@ class Purchase(models.Model):
     person = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
+
+class PromoCode(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount_percent = models.PositiveSmallIntegerField(choices=[(5, '5%'), (10, '10%'), (15, '15%')])
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.code} ({self.discount_percent}%)"
