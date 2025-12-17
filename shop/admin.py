2bd479811cf38d48fd3ce51e91f0c297e25a7868
Anton Ostapko <ghotom1@mail.ru>
2025-12-18 00:01:25 +0300

    add admins classes

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Product, Purchase, PromoCode
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
+
+@admin.register(PromoCode)
+class PromoCodeAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount_percent', 'active')
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('person', 'product', 'address', 'date')
+    list_filter = ('date',)
