802f10cafd5bbfa13497e36cdfc962d4e81f80f4
Anton Ostapko <ghotom1@mail.ru>
2025-12-18 00:03:08 +0300

    add interface of promocode form

---
+++ shop/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+class PromoCodeForm(forms.Form):
+    code = forms.CharField(
+        max_length=20,
+        required=False,
+        label="Промокод",
+        widget=forms.TextInput(attrs={'placeholder': 'Введите промокод'})
+    )
